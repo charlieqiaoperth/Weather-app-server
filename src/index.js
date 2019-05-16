2cca79b0a786487948a5d7efb9b06cb242aa98e7
silverwind <charlieqiaoperth@gmail.com>
2019-05-16 11:24:22 +0800

    helmet and cors problem done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
 const notFound= require('./middleware/notFound');
 const errorHandler = require('./middleware/errorHandler');
 require('dotenv').config();
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 5000;
+ 
+app.use(helmet());
+app.use(cors());
 
 app.use(routes);
 app.use(errorHandler);
 app.use(notFound);
 
 app.listen(PORT, () =>{
-    console.log(`server listen on PORT ${PORT}`);
+    console.log(`server listen on PORT ${PORT}`);n
 });
