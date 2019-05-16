c7d8d536cec51e7b82bea4868665dc2fda29391f
silverwind <charlieqiaoperth@gmail.com>
2019-05-16 21:58:55 +0800

    ready to deploy

--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -8,7 +8,7 @@ const Weather = require('../models/Weather');
 function getRowData(req,res,next) {
     const { cc, city } = req.params; 
     const weatherType = req.query.weatherType;
-    console.log(weatherType)   ;
+    // console.log(weatherType)   ;
    Weather.getData(city,cc,weatherType)
    .then(response => responseFormatter(res, 200, null, response))
    .catch(err =>next(err)); 
